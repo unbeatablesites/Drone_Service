d5f4b8ce94fc6efed76ebf1651f9721b64e0f02b
Frank Uzoka <Frankuzoka@hotmail.com>
2020-12-01 23:40:01 -0500

    Add starter files

---
+++ DroneAppBackEnd/src/main/java/com/droneBE/DroneAppBackEnd/DroneAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.droneBE.DroneAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneAppBackEndApplication.class, args);
+	}
+
+}
